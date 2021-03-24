2db33b23eb24b408f99adc154880dc99bb17e4d9
daniyal tariq <aasma@Aasmas-MacBook-Pro.local>
2021-03-24 22:18:59 +0500

    web pages css has been improved

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'weather app',
+        title: 'Weather',
         description: 'made through handlebars',
         name: 'Daniyal'
     })
