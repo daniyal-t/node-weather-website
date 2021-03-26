7c82a63f9aa5548bc1dc438d997dab4970d704ab
daniyal tariq <aasma@Aasmas-MacBook-Pro.local>
2021-03-26 21:28:15 +0500

    fetching data through form and displaying weather on home page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-console.log('i see i see why you are here')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error
+            console.log(data.error)
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecastData 
+            console.log(data.location)
+            console.log(data.forecastData)
+            console.log(data.longitude)
+            console.log(data.latitude)
+            document.getElementById('search').value=null
+        }
+    })
+})
+})
